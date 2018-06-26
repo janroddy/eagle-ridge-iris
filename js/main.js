9fce2d7977e949aecb65ad3aabbf663522c1969f
Jan Roddy <jan.roddy.e@gmail.com>
2018-06-25 22:33:57 -0600

    Added responsive drop down nav bar with JS

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//$(document).ready(function(){
+ /*
+	var photoAlbum = ["img/iris1.jpg", "img/iris2.jpg", "img/iris3.jpg", "img/iris4.jpg", "img/iris5.jpg", "img/iris6.jpg"];
+	var i = 0;
+	var changePic = function () {
+		i++;
+		if(i == photoAlbum.length) i = 0;
+		$(#"gallery").src = photoAlbum[i];
+	};
+
+	$("#gallery".click(changePic); */
+
+	
+//};
+
+// {;
+	
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
